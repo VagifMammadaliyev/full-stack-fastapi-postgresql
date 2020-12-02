462bffe8f93db8491abe5f49ebbddbff2b3c74c6
VagifMammadaliyev <vagifmammadaliyev@outlook.com>
2020-12-02 04:01:53 +0400

    Removed Docker swapm deployment related things
    Deployment process is incomplete for now. Just building containers and
    thats all...

--- {{cookiecutter.project_slug}}/scripts/deploy.sh
+++ {{cookiecutter.project_slug}}/scripts/deploy.sh
@@ -4,13 +4,7 @@
 set -e
 
 DOMAIN=${DOMAIN?Variable not set} \
-TRAEFIK_TAG=${TRAEFIK_TAG?Variable not set} \
-STACK_NAME=${STACK_NAME?Variable not set} \
 TAG=${TAG?Variable not set} \
-docker-compose \
--f docker-compose.yml \
-config > docker-stack.yml
 
-docker-auto-labels docker-stack.yml
-
-docker stack deploy -c docker-stack.yml --with-registry-auth "${STACK_NAME?Variable not set}"
+# Simple deployment is just to build and start docker containers
+docker-compose -f docker-compose.yml up -d
